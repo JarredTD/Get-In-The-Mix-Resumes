2b2584bcd4cdaa23f27523a09c3a7780576256e3
JarredTD <jarred.desrosiers@yahoo.com>
2024-03-21 12:53:06 -0400

    init: Create initial structure and scripts

---
+++ README.md
@@ -0,0 +1,12 @@
+# Instructions (Mac Only)
+
+## Running
+
+1. `source ./scripts/deploy.sh`
+2. Go to outputted address
+
+## Developing
+
+1. `python -m venv venv`
+2. `source ./venv/bin/activate`
+3. pip3 install -r ./app/requirements.txt
---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+clear
+
+source ./scripts/build.sh
+
+clear
+
+docker build -t gitmr-app ./app/
+
+if docker container inspect gitmr-app &> /dev/null; then
+    echo "Removing existing gitmr-app container..."
+    docker container rm -f gitmr-app
+fi
+
+docker run -d --name gitmr-app -p 8080:8080 gitmr-app
+
+echo "gitmr-app container is up and running on 127.0.0.1:8080"
