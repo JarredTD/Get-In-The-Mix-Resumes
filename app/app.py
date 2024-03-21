dbcef65509f0bc009ce71bd6bca75d3de1f6acbb
JarredTD <jarred.desrosiers@yahoo.com>
2024-03-21 13:57:43 -0400

    feat: Add routing to all 3 pages

--- app/app.py
+++ app/app.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template("index.html")
+
+@app.route("/about-us")
+def about_us():
+    return render_template("about_us.html")
+
+@app.route("/project-motivation")
+def motivation():
+    return render_template("motivation.html")
