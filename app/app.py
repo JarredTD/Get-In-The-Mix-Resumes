7e2f78842ba8e0c87e177713edb8fa8e8b320187
JarredTD <jarred.desrosiers@yahoo.com>
2024-03-22 09:07:24 -0400

    docs: Add doc-strings

--- app/app.py
+++ app/app.py
@@ -1,3 +1,5 @@
+"""Entry point for the flask application"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,12 +7,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """Routes '/' to 'index.html'"""
     return render_template("index.html")
+
 
 @app.route("/about-us")
 def about_us():
+    """Routes '/about-us' to 'about_us.html'"""
     return render_template("about_us.html")
+
 
 @app.route("/project-motivation")
 def motivation():
+    """Routes '/project-motivation' to 'motivation.html'"""
     return render_template("motivation.html")
