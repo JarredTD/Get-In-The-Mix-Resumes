c38728012a4205d24134b78767ca5dd6f702cf0a
JarredTD <jarred.desrosiers@yahoo.com>
2024-03-23 01:58:39 -0400

    docs: Update doc comments on tests to use autodoc format

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,38 +1,46 @@
-"""Unit tests for app.py"""
-
-# pylint: disable=import-error
-# pylint: disable=no-name-in-module
+"""Unit tests for the Flask application defined in app.py."""
 
 import unittest
 from app.app import app
 
+# pylint: disable=import-error
+# pylint: disable=no-name-in-module
+
 
 class FlaskTestCase(unittest.TestCase):
-    """Test suite for app"""
+    """
+    A collection of unit tests for the Flask application.
+
+    This suite tests the routing and responses for various pages within the application.
+    """
 
     def setUp(self):
-        """Creates test client of app"""
+        """
+        Prepare the test client for the Flask application.
+        """
         self.app = app.test_client()
         self.app.testing = True
 
     def test_index(self):
-        """Test index page routing"""
+        """
+        Verify that the index page returns a status code of 200 and the content type is HTML.
+        """
         response = self.app.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertIn("text/html", response.content_type)
 
     def test_about_us(self):
-        """Test about us page routing"""
+        """
+        Ensure the 'About Us' page is accessible and returns HTML content.
+        """
         response = self.app.get("/about-us")
         self.assertEqual(response.status_code, 200)
         self.assertIn("text/html", response.content_type)
 
     def test_motivation(self):
-        """Test project motivation page routing"""
+        """
+        Test that the 'Project Motivation' page is routed correctly and serves HTML.
+        """
         response = self.app.get("/project-motivation")
         self.assertEqual(response.status_code, 200)
         self.assertIn("text/html", response.content_type)
-
-
-if __name__ == "__main__":
-    unittest.main()
