337393bb133c4d66e0fe4751f73cefa0b676ae70
JarredTD <jarred.desrosiers@yahoo.com>
2024-03-25 02:56:33 -0400

    feat: Add flask_migrate and flask_sqlalchemy to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 """Declare Flask App"""
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from app.config import DATABASE_URI
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# pylint: disable=wrong-import-position
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app.scripts import models
 from app.scripts import views
