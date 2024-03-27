b2be8dae5abe2b4c756c1a5d3caad31dae909727
JarredTD <jarred.desrosiers@yahoo.com>
2024-03-27 01:49:57 -0400

    feat: Add verbose option to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,18 +4,25 @@ source venv/bin/activate
 
 set -e
 
-# Define paths for tools
 BLACK_PATH="build/tools/black"
 PYLINT_PATH="build/tools/pylint"
 PYTEST_PATH="build/tools/pytest"
 
-# Ensure the necessary tool directories exist
 mkdir -p "$BLACK_PATH" "$PYLINT_PATH" "$PYTEST_PATH"
+
+# Check if '-v' is passed as an argument
+VERBOSE=""
+for arg in "$@"
+do
+    if [ "$arg" = "-v" ]; then
+        VERBOSE="--verbose"
+    fi
+done
 
 echo " "
 echo "--------- BLACK ---------------------------"
 echo " "
-if ! black src/app src/tests --config pyproject.toml --verbose 2>&1 | tee "$BLACK_PATH/black_output.txt"; then
+if ! black src/app src/tests --config pyproject.toml $VERBOSE 2>&1 | tee "$BLACK_PATH/black_output.txt"; then
     echo "Black found format issues or encountered an error."
     exit 1
 fi
@@ -23,7 +30,7 @@ fi
 echo " "
 echo "--------- PYLINT ---------------------------"
 echo " "
-if ! pylint --rcfile pyproject.toml src/app src/tests --verbose 2>&1 | tee "$PYLINT_PATH/pylint_output.txt"; then
+if ! pylint --rcfile pyproject.toml src/app src/tests $VERBOSE 2>&1 | tee "$PYLINT_PATH/pylint_output.txt"; then
     echo "Pylint encountered issues or errors."
     exit 1
 fi
@@ -31,7 +38,7 @@ fi
 echo " "
 echo "--------- PYTEST /W COVERAGE ---------------------------"
 echo " "
-if ! pytest 2>&1 | tee "$PYTEST_PATH/pytest_output.txt"; then
+if ! pytest $VERBOSE 2>&1 | tee "$PYTEST_PATH/pytest_output.txt"; then
     echo "Pytest encountered errors."
     exit 1
 fi
